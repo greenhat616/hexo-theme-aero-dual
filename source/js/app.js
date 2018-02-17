b4de59905a48aff5174bed864abc2fa89155c052
a632079 <a632079@qq.com>
2018-02-17 21:53:47 +0800

    Async Load Script

--- source/js/app.js
+++ source/js/app.js
@@ -1,4 +1,4 @@
-
+// Write Hitokoto
 function write(text) {
     if (text.length < 40) {
         $('#hitokoto').text(text);
@@ -7,13 +7,15 @@ function write(text) {
     }
 };
 
+
+// Auto Request Hitokoto
 (function gethitokoto() {
     $.ajax({
         url: "https://api.a632079.me/?encode=json",
         dataType: "jsonp",
         async: true,
         jsonp: "callback",
-        jsonpCallback: "echokoto",
+        jsonpCallback: "moecat",
         success: function (result) {
             write(result.hitokoto);
         },
@@ -23,4 +25,35 @@ function write(text) {
     });
 })();
 
+// Async Load Header Banner
+(function asyncLoadBanner() {
+    if (typeof BannerURL == 'undefined') {
+        // Something wrong
+        console.log('[Banner] 未发现 Banner ， 加载默认图片...');
+        var ts = Date.now(); // Get TS
+
+        // Async Image
+        var BannerIMG = new Image();
+        BannerIMG.src = 'https://piccdn.freejishu.com/images/2017/08/22/qerIg.png!/format/jpg';
+        BannerIMG.onload = function () {
+            // Image Load Done
+            document.getElementsByClassName('header-background')[0].style.backgroundImage = ('url(' + BannerIMG.src + ')');
+            console.log('[Banner] 图片加载完成， 耗时： %s ms', (Date.now() - ts));
+        }
+
+    } else {
+        console.log('[Banner] 发现 Banner ， 加载图片...')
+        var ts = Date.now(); // Get TS
+
+        // Async Image
+        var BannerIMG = new Image();
+        BannerIMG.src = BannerURL;
+        BannerIMG.onload = function () {
+            // Image Load Done
+            document.getElementsByClassName('header-background')[0].style.backgroundImage = ('url(' + BannerURL + ')');
+            console.log('[Banner] 图片加载完成， 耗时： %s ms', (Date.now() - ts));
+        }
+
+    }
+})();
 
