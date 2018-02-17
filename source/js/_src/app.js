aaaa19ac2415133136ee6a17e0749f46c6739179
a632079 <a632079@qq.com>
2018-02-18 01:46:56 +0800

    Async Comment and Webpacked JavaScript

--- source/js/_src/app.js
+++ source/js/_src/app.js
@@ -1,54 +1,76 @@
+// Import Polyfill
+import 'babel-polyfill'
+
 // Write Hitokoto
-function write(text) {
-        $('#hitokoto').text(text);
-};
+if (typeof window.$ === 'undefined') {
+  throw new Error('JQuery is not loaded!')
+}
+const write = text => {
+  if (text.length < 20) {
+    gethitokoto()
+  } else {
+    document.getElementById('hitokoto').textContent = text
+  }
+}
 
 // Auto Request Hitokoto
-(function gethitokoto() {
-    $.ajax({
-        url: "https://api.a632079.me/?encode=json",
-        dataType: "jsonp",
-        async: true,
-        jsonp: "callback",
-        jsonpCallback: "moecat",
-        success: function (result) {
-            write(result.hitokoto);
-        },
-        error: function () {
-            write("读取一言数据失败了的说……_(:з」∠)_");
-        }
-    });
-})();
+const gethitokoto = () => {
+  window.$.ajax({
+    url: 'https://api.a632079.me/?encode=json',
+    dataType: 'jsonp',
+    async: true,
+    jsonp: 'callback',
+    jsonpCallback: 'moecat',
+    success: result => {
+      write(result.hitokoto)
+    },
+    error: () => {
+      write('读取一言数据失败了的说……_(:з」∠)_')
+    }
+  })
+}
 
 // Async Load Header Banner
-(function asyncLoadBanner() {
-    if (typeof BannerURL == 'undefined') {
-        // Something wrong
-        console.log('[Banner] 未发现 Banner ， 加载默认图片...');
-        var ts = Date.now(); // Get TS
 
-        // Async Image
-        var BannerIMG = new Image();
-        BannerIMG.src = 'https://piccdn.freejishu.com/images/2017/08/22/qerIg.png!/format/jpg';
-        BannerIMG.onload = function () {
-            // Image Load Done
-            document.getElementsByClassName('header-background')[0].style.backgroundImage = ('url(' + BannerIMG.src + ')');
-            console.log('[Banner] 图片加载完成， 耗时： %s ms', (Date.now() - ts));
-        }
+const asyncLoadBanner = () => {
+  window.BannerIMG = (typeof BannerURL === 'undefined') ? 'https://piccdn.freejishu.com/images/2017/08/22/qerIg.png!/format/jpg' : window.BannerIMG
+  console.log('[Banner] 发现 Banner ， 加载图片...')
+  const ts = Date.now() // Get TS
 
-    } else {
-        console.log('[Banner] 发现 Banner ， 加载图片...')
-        var ts = Date.now(); // Get TS
+  // Async Image
+  const BannerIMG = new Image()
+  BannerIMG.src = window.BannerURL
+  BannerIMG.onload = function () {
+    // Image Load Done
+    document.getElementsByClassName('header-background')[0].style.backgroundImage = ('url(' + window.BannerURL + ')')
+    console.log('[Banner] 图片加载完成， 耗时： %s ms', (Date.now() - ts))
+  }
+}
 
-        // Async Image
-        var BannerIMG = new Image();
-        BannerIMG.src = BannerURL;
-        BannerIMG.onload = function () {
-            // Image Load Done
-            document.getElementsByClassName('header-background')[0].style.backgroundImage = ('url(' + BannerURL + ')');
-            console.log('[Banner] 图片加载完成， 耗时： %s ms', (Date.now() - ts));
-        }
+// Async Load Valine
+const asyncLoadValine = config => {
+  if (!config) return
+  const urls = [`https://static.a632079.me/tools/av-min/3.0.4/av-min.js`, `https://static.a632079.me/tools/valine/1.1.9-beta4/Valine.min.js`]
+  const asyncloader = url =>
+    new Promise((resolve, reject) => {
+      const script = document.createElement('script')
+      script.src = url
+      script.addEventListener('load', _ => resolve(), false)
+      script.addEventListener('error', _ => reject(_), false)
+      document.body.appendChild(script)
+    })
+  console.log('开始加载 Valine ...')
+  const ts = Date.now()
+  Promise.all(urls.map(asyncloader))
+    .then(_ => {
+      document.getElementById('comment-hr').style.display = 'block'
+      new window['Valine'](config)
+      console.log('Valine 加载完成, 耗时: %s ms', (Date.now() - ts))
+    })
+    .catch(e => console.error('加载 Valine 失败,', e));
+}
 
-    }
-})();
-
+// Run Function
+gethitokoto()
+asyncLoadBanner()
+asyncLoadValine(window['ValineConfig'])
---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+const path = require('path');
+
+module.exports = {
+  entry: './source/js/_src/app.js',
+  output: {
+    path: path.join(__dirname, './source/js/'),
+    filename: 'app.js',
+  },
+  module: {
+    rules: [
+      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
+    ]
+  }
+  
+};
